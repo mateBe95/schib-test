c7f3a36cad8df1f063af4a53e445e540eeb92c70
Mateusz <mateusz.bieda95@gmail.com>
2020-03-27 22:40:36 +0100

    add webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,43 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
     devServer: {
         contentBase: './dist',
         port: 3000
     },
-    plugins: [new HtmlWebpackPlugin()]
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            }
+          },
+          {
+            test: /\.html$/,
+            use: [
+              {
+                loader: "html-loader"
+              }
+            ]
+          },
+          {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          },
+        ]
+      },
+    plugins: [
+      new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ]
+
 };
