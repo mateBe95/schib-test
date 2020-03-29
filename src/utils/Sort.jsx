2cb8faff5391fb070dfd89c109e49ac26e725f28
Mateusz <mateusz.bieda95@gmail.com>
2020-03-29 17:53:00 +0200

    fix sorting news by publication date

--- src/utils/Sort.jsx
+++ src/utils/Sort.jsx
@@ -1,12 +1,34 @@
+const MONTHS = [
+  "januar",
+  "februar",
+  "march",
+  "april",
+  "mai",
+  "june",
+  "july",
+  "august",
+  "september",
+  "oktober",
+  "november",
+  "desember"
+];
+
+const formatDate = ( date ) => {
+  const DATE =  date.date || date 
+  const [day, monthName, year] = DATE.replace('.', '').split(" ");
+  const month = MONTHS.indexOf(monthName);
+  return new Date(year, month, day);
+};
+
 const SORT_UTIL = {
   increase: {
-    sortBy: (a, b) => new Date(a.date) - new Date(b.date)
+    sortBy: (a, b) => formatDate(a.date) - formatDate(b.date)
   },
   decrease: {
-    sortBy: (a, b) => new Date(b.date) - new Date(a.date)
+    sortBy: (a, b) => formatDate(b.date) - formatDate(a.date)
   },
   default: {
-    sortBy: (a, b) => new Date(a)
+    sortBy: (a) => formatDate(a)
   }
 };
 
